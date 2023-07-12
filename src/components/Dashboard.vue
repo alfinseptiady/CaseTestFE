b0e0a3a0e4089c1aca7edb887f0ace624b04783e
Alfin Eria Septiady <58551028+alfinseptiady@users.noreply.github.com>
2023-07-12 10:41:32 +0700

    updateDataBarang

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="container mx-auto">
+  <div class="container ml-10">
     <h1 class="text-2xl font-bold mt-8">Dashboard</h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="getAllData">
-      Get All Data
-    </button>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="this.$router.push('/tambahbarang')">
-      tambah Data
+    <button
+      class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      @click="this.$router.push('/tambahbarang')">
+      Tambah Data
     </button>
     <!-- <ul>
       <li v-for="item in data.data" :key="item.id">
@@ -16,22 +15,34 @@
       </li>
     </ul>
     {{ JSON.stringify(data) }} -->
-
-    <table class="shadow-lg bg-white"  v-for="item in data.data" :key="item.id">
-  <tr>
-    <th class="bg-blue-100 border text-left px-8 py-4">Nama Barang</th>
-    <th class="bg-blue-100 border text-left px-8 py-4">Harga</th>
-    <th class="bg-blue-100 border text-left px-8 py-4">Stok</th>
-  </tr>
-  <tr>
-    <td class="border px-8 py-4">{{ item.namaBarang }}</td>
-    <td class="border px-8 py-4">{{ item.harga }}</td>
-    <td class="border px-8 py-4">{{ item.stok }}</td>
-  </tr>
-  
-</table>
+    <table class="shadow-lg bg-white mt-3">
+      <tr>
+        <th class="bg-blue-100 border px-8 py-4 text-center">ID data</th>
+        <th class="bg-blue-100 border px-8 py-4 text-center">Nama Barang</th>
+        <th class="bg-blue-100 border px-8 py-4 text-center">Harga</th>
+        <th class="bg-blue-100 border px-8 py-4 text-center">Stock</th>
+        <th class="bg-blue-100 border px-8 py-4 text-center">Nama Supplier</th>
+        <th class="bg-blue-100 border px-8 py-4 text-center">No Telp</th>
+        <th class="bg-blue-100 border px-8 py-4 text-center">Alamat</th>
+        <th class="bg-blue-100 border px-8 py-4 text-center">Actions</th>
+      </tr>
+      <!-- {{ console.log(data.data) }} -->
+      <tr v-for="item in data.data" :key="item.id">
+        <td class="border px-8 py-4">{{ item.id }}</td>
+        <td class="border px-8 py-4">{{ item.namaBarang }}</td>
+        <td class="border px-8 py-4">{{ item.harga }}</td>
+        <td class="border px-8 py-4">{{ item.stok }}</td>
+        <td class="border px-8 py-4">{{ item.supplier.namaSupplier || "-" }}</td>
+        <td class="border px-8 py-4">{{ item.supplier.noTelp || "-" }}</td>
+        <td class="border px-8 py-4">{{ item.supplier.alamat || "-" }}</td>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-3 ml-2"
+          @click="this.$router.push(`/editbarang/${item.id}`)">
+          Edit
+        </button>
+      </tr>
+    </table>
   </div>
-  
 </template>
 
 <script>
@@ -43,33 +54,30 @@ export default {
       data: [],
     };
   },
-  methods: {
-    getAllData() {
-      const limit = 20;
-      const offset = 1;
-      axios
-        .get('http://159.223.57.121:8090/barang/find-all', {
-          params: {
-            limit,
-            offset,
-          },
-          headers: {
-            Authorization: `Bearer ${ localStorage.getItem('token')}`,
-          }
-        })
-        .then(response => {
-          this.data = response.data;
-          // Lakukan tindakan dengan data yang diperoleh, seperti menampilkan data ke pengguna
-        })
-        .catch(error => {
-          console.error(error.response.data);
-          // Tangani kesalahan saat mengambil data, seperti menampilkan pesan kesalahan
-        });
-    },
-  },
+
+  mounted() {
+    const limit = 5;
+    const offset = 1;
+    axios
+      .get('http://159.223.57.121:8090/barang/find-all', {
+        params: {
+          limit,
+          offset,
+        },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        }
+      })
+      .then(response => {
+        this.data = response.data;
+      })
+      .catch(error => {
+        console.error(error.response.data);
+        alert("Gagal Menampilkan Data");
+      });
+    // axios
+    //   .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    //   .then(response => (this.info = response))
+  }
 };
 </script>
-
-<style>
-/* Tambahkan gaya khusus jika diperlukan */
-</style>
